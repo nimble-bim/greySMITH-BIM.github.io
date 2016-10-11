26bb37e426a4473d7ea37ce23fa132f199f94037
Preston (desktop) <smith.preston2@gmail.com>
2016-10-11 09:34:56 -0600

    Animate Contact link

--- assets/js/app/system/animations.js
+++ assets/js/app/system/animations.js
@@ -10,5 +10,19 @@
                 500);
             });
 
+    $('#contact')
+        .hover(
+            function() {
+                $(this).fadeOut(500,
+                function() {
+                    $(this).text('info@greysmith.com').fadeIn(250);});
+            },
+            function() {
+                $(this).fadeOut(500,
+                    function() {
+                        $(this).text('Contact Us.').fadeIn(150);
+                    }
+            )});
+    console.log($);
 
 })();
