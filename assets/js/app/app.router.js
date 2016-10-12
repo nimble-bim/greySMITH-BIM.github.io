fd3edfb6be264b34dc2644a270bdf225a7ac8181
Preston (desktop) <smith.preston2@gmail.com>
2016-10-12 14:19:36 -0600

    Add simple angular router | fix all page routing

---
+++ assets/js/app/app.router.js
@@ -0,0 +1,19 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(router);
+
+    function router($routeProvider, $locationProvider) {
+
+        $locationProvider.hashPrefix('');
+
+        $routeProvider
+            .when('/', { templateUrl: 'sections/Home.html', controller: 'mainController', controllerAs: 'vm' })
+            .when('/About', { templateUrl: 'sections/About.html', controller: 'aboutController', controllerAs: 'vm' })
+            .when('/Products', { templateUrl: 'sections/Products.html', controller: 'productController', controllerAs: 'vm' })
+            .when('/News', { templateUrl: 'sections/News.html', controller: 'newsController', controllerAs: 'vm' })
+            .otherwise('/');
+    }
+})();
