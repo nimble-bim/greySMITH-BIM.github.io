83c8ee096993787c81be1019d455620b77510874
Preston (desktop) <smith.preston2@gmail.com>
2016-10-12 15:34:49 -0600

    Add controller

---
+++ assets/js/app/app.controller.js
@@ -0,0 +1,16 @@
+﻿(function() {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('aboutController', aboutController);
+
+    function aboutController($scope, $location, $anchorScroll) {
+        $scope.scrollTo = (function(id) {
+            $location.hash(id);
+            $anchorScroll();
+        });
+    }
+
+})();
--- assets/js/app/app.router.js
+++ assets/js/app/app.router.js
@@ -11,6 +11,7 @@
 
         $routeProvider
             .when('/', { templateUrl: 'sections/Home.html', controller: 'mainController', controllerAs: 'vm' })
+            .when('/_shell', { templateUrl: 'sections/_Shell.html', controller: 'otherController', controllerAs: 'vm' })
             .when('/About', { templateUrl: 'sections/About.html', controller: 'aboutController', controllerAs: 'vm' })
             .when('/Products', { templateUrl: 'sections/Products.html', controller: 'productController', controllerAs: 'vm' })
             .when('/News', { templateUrl: 'sections/News.html', controller: 'newsController', controllerAs: 'vm' })
