b1b30782381ec2743c8dc1f2462e8b3a90a7f7cf
Preston Smith <PrestonII@users.noreply.github.com>
2016-10-13 06:32:36 -0600

    Add angular routing
    * Add simple angular router | fix all page routing
    
    * Add controller
    
    * Add about module with it's own controller
    
    * Add navbar
    
    * Separate into two pages Shell | News
    
    * Temporarily remove GS Head

---
+++ assets/js/app/app.module.js
@@ -0,0 +1,11 @@
+﻿(function() {
+
+    angular
+        .module('app',
+        [
+            'ngRoute',
+
+            'app.about'
+        ]);
+
+})();
--- assets/js/app/application.js
+++
@@ -1,6 +0,0 @@
-﻿(function() {
-
-    angular
-        .module('application', []);
-
-})();
