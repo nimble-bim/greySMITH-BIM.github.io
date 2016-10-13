20aad28ddd8c710de4109c038eb33d471e491530
Preston (desktop) <smith.preston2@gmail.com>
2016-10-12 18:12:21 -0600

    Separate into two pages Shell | News

--- assets/js/app/app.router.js
+++ assets/js/app/app.router.js
@@ -10,10 +10,7 @@
         $locationProvider.hashPrefix('');
 
         $routeProvider
-            .when('/', { templateUrl: 'sections/Home.html', controller: 'mainController', controllerAs: 'vm' })
-            .when('/_shell', { templateUrl: 'sections/_Shell.html', controller: 'otherController', controllerAs: 'vm' })
-            .when('/About', { templateUrl: 'sections/About.html', controller: 'aboutController', controllerAs: 'vm' })
-            .when('/Products', { templateUrl: 'sections/Products.html', controller: 'productController', controllerAs: 'vm' })
+            .when('/', { templateUrl: 'sections/_Shell.html', controller: 'mainController', controllerAs: 'vm' })
             .when('/News', { templateUrl: 'sections/News.html', controller: 'newsController', controllerAs: 'vm' })
             .otherwise('/');
     }
