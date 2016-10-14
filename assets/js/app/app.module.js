e3e9bd6e8cb2740731c8e8bdd132fd30d58c5231
Preston (desktop) <smith.preston2@gmail.com>
2016-10-13 18:53:47 -0600

    Remove all markdown material

--- assets/js/app/app.module.js
+++ assets/js/app/app.module.js
@@ -3,7 +3,7 @@
     angular
         .module('app',
         [
-            'ngRoute', 'btford.markdown',
+            'ngRoute', 
 
             'app.about', 'app.news'
         ]);
--- assets/js/app/news/news.controller.js
+++ assets/js/app/news/news.controller.js
@@ -12,7 +12,7 @@
         var newsVm = this;
         newsVm.name = {};
         newsVm.posts = [
-            { title: "Working With Art As Education.", mainImageUrl: '../assets/images/ACE_cover.png' },
+            { title: "Working With Art As Education.", mainImageUrl: '../assets/images/ACE_cover.png', markdown: "#Should Be Title"},
             { title: "Why The Majority of BIM Projects Aren't Really BIM Projects.", mainImageUrl: '../assets/images/pasted1.png' },
             { title: "NIMBLE. (Or: How I Survived After Working With The Worst Team In The World).", mainImageUrl: '../assets/images/corbour_02.png' }
         ];
