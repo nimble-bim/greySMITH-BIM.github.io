1af9363bb51683f7c511edc64660b2a747709131
Preston (desktop) <smith.preston2@gmail.com>
2016-10-20 17:51:06 -0600

    Add layout for archive section

--- assets/js/app/news/news.controller.js
+++ assets/js/app/news/news.controller.js
@@ -11,32 +11,10 @@
 
         var newsVm = this;
         newsVm.name = {};
-        newsVm.findPosts = findPosts;
-        newsVm.findPostContent = findPostContent;
         newsVm.posts = [
             { location: 'blog/2016/10/13/angular-blog-test.html'},
             { location: 'blog/2016/10/08/from-draft.html'},
             { location: 'blog/2016/10/08/windows-version.html'}
         ];
-
-        function findPostContent(fileLocation) {
-            var rawFile = new XMLHttpRequest();
-            var allText = {};
-            rawFile.open("GET", fileLocation, false);
-
-            rawFile.send();
-
-            if (rawFile.status === 200 || rawFile.status === 0) {
-                allText = rawFile.responseText;
-                return allText;
-            }
-
-            console.log("can't find file");
-
-            return allText;
-        }
-        function findPosts() {
-
-        };
     }
 })();
