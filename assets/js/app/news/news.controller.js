3ec8ca2a92d00cf9ce30db8f5bf6a2fd6656bf9a
Preston (desktop) <smith.preston2@gmail.com>
2016-10-20 15:06:56 -0600

    Resolve blog building problems

--- assets/js/app/news/news.controller.js
+++ assets/js/app/news/news.controller.js
@@ -5,18 +5,18 @@
         .module('app.news')
         .controller('newsController', newsController);
 
+    newsController.$inject = ['$http', '$templateCache'];
 
-
-    function newsController() {
+    function newsController($http, $templateCache) {
 
         var newsVm = this;
         newsVm.name = {};
         newsVm.findPosts = findPosts;
         newsVm.findPostContent = findPostContent;
         newsVm.posts = [
-            { title: "Working With Art As Education.", mainImageUrl: "../assets/images/ACE_cover.png", contentLocation: 'license.txt', content: {}},
-            { title: "Why The Majority of BIM Projects Aren't Really BIM Projects.", mainImageUrl: '../assets/images/pasted1.png' },
-            { title: "NIMBLE. (Or: How I Survived After Working With The Worst Team In The World).", mainImageUrl: '../assets/images/corbour_02.png' }
+            { location: 'blog/2016/10/13/angular-blog-test.html'},
+            { location: 'blog/2016/10/08/from-draft.html'},
+            { location: 'blog/2016/10/08/windows-version.html'}
         ];
 
         function findPostContent(fileLocation) {
@@ -28,7 +28,7 @@
 
             if (rawFile.status === 200 || rawFile.status === 0) {
                 allText = rawFile.responseText;
-                return;
+                return allText;
             }
 
             console.log("can't find file");
