7070d2408cb69100d746e303e4ec9ae4dcd1cccd
Preston (desktop) <smith.preston2@gmail.com>
2016-10-24 14:29:07 -0600

    Fix scrolling

--- assets/js/app/app.controller.js
+++ assets/js/app/app.controller.js
@@ -7,22 +7,29 @@
         .controller('mainController', mainController);
 
 
-    function mainController($scope, $location, $anchorScroll) {
+    function mainController($scope, $location) {
         var vm = this;
         vm.navigateTo = navigateTo;
-
-        $anchorScroll.yOffset = 150;
 
         function navigateTo(page, id) {
             var path = $location.path();
 
             console.log(path);
 
-            if($location.path() !== page)
+            if ($location.path() !== page) {
+                //$location.hash(page);
                 $location.path(page);
+            }
 
-            $anchorScroll(id);
+            scrollPage(id);
+        }
+
+        function scrollPage(id) {
+            var idSelector = $("#" + id);
+            console.log($);
+            console.log(id);
+                
+            $('body').animate({ scrollTop: idSelector.offset().top - 150 }, "slow");
         }
     }
-
 })();
